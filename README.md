de6e8c737fe663ec7a2d33fa145998e7a0dbd0a9
Chris Zubak-Skees <chriszs@gmail.com>
2018-07-09 10:11:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# tax-race
+
+> 
+
+## Build setup
+
+``` bash
+# install dependencies
+$ yarn
+
+# serve with hot reload at localhost:3000/tax-race
+$ yarn dev
+
+# build for production and launch server
+$ yarn start
+
+# or generate static page
+$ yarn generate
+
+# and push to s3
+$ yarn push
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+        <h1>
+            EXAMPLES
+        </h1>
+        <ul class="examples">
+            <li v-for="(example, index) in examples" :key="index" class="example">
+                <nuxt-link :to="{ name: 'id', params: { id: index }}">
+                    {{ example.name }}
+                </nuxt-link>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+import examples from '~/assets/examples.json';
+
+export default {
+    data () {
+        return examples;
+    },
+    head () {
+        return {
+            title: 'examples'
+        };
+    }
+};
+</script>
+
+<style scoped>
+.examples {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.example {
+    margin: 10px 0;
+}
+</style>
